bea003129327b1773763517af73db0f1b4f3fd61
Felipe Rabuske Costa <felipe.costa@sap.com>
2020-12-02 18:23:52 -0300

    Day 02 part 02

--- src/day02/day02.service.ts
+++ src/day02/day02.service.ts
@@ -17,13 +17,19 @@ export class Day02Service {
   processInput(): string {
     const fileContent =  this.fileReaderService.readFileAsString("./input/day02.txt");
     const passwords = fileContent.split("\n").map(line => this.convertLineToPassword(line));
-    return passwords.filter(password => this.doesPasswordFollowPolicy(password)).length.toString();
-
+    const part01 = passwords.filter(password => this.doesPasswordFollowPolicy01(password)).length.toString();
+    const part02 = passwords.filter(password => this.doesPasswordFollowPolicy02(password)).length.toString();
+    return `Part 01 [${part01}] Part 02 [${part02}]`;
   }
 
-  doesPasswordFollowPolicy(password: PolicyAndPassword) : boolean {
+  doesPasswordFollowPolicy01(password: PolicyAndPassword) : boolean {
     const numberOfTimesCharAppears = password.password.split("").filter(char => char === password.checkChar).length;
     return password.lowerRange <= numberOfTimesCharAppears && numberOfTimesCharAppears <= password.higherRange;
+  }
+
+  doesPasswordFollowPolicy02(password: PolicyAndPassword) : boolean {
+    return (password.password.charAt(password.lowerRange - 1) === password.checkChar) !== // This acts as a XOR between the two booleans
+           (password.password.charAt(password.higherRange - 1) === password.checkChar);
   }
 
   convertLineToPassword(line: string) : PolicyAndPassword {
