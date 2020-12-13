0f2c2407aa4345626f82458220947a36fa0475f5
Felipe Rabuske Costa <felipe.costa@sap.com>
2020-12-13 20:02:15 -0300

    Small refactor Day 12 🎅

--- src/day12/day12.service.ts
+++ src/day12/day12.service.ts
@@ -4,48 +4,31 @@ import Point from 'src/utils/point';
 
 // https://adventofcode.com/2020/day/12
 
-enum Direction {
-  NORTH,
-  SOUTH,
-  EAST,
-  WEST,
-}
-
 interface Action {
   id: string;
   value: number;
 }
 
 class Ship {
-  private _currentDirection: Direction;
   private _currentWayPoint: Point;
   private _currentPosition: Point; // X is the east/west, Y is the north/south
-  private static _directionOrder = [Direction.EAST, Direction.SOUTH, Direction.WEST, Direction.NORTH];
-
-  constructor() {
-    this._currentDirection = Direction.EAST;
+ 
+  constructor(wayPoint: Point) {
     this._currentPosition = new Point(0, 0);
-    this._currentWayPoint = new Point(10, 1);
+    this._currentWayPoint = wayPoint;
   }
 
-  private _deriveNavigationPoint(direction: Direction, value: number): Point {
+  private _deriveNavigationPoint(direction: string, value: number): Point {
     switch (direction) {
-      case Direction.SOUTH:
+      case 'S':
         return new Point(0, -value);
-      case Direction.NORTH:
+      case 'N':
         return new Point(0, value);
-      case Direction.EAST:
+      case 'E':
         return new Point(value, 0);
-      case Direction.WEST:
+      case 'W':
         return new Point(-value, 0);
     }
-  }
-
-  private _updateDirection(numberOfTurnsAndDirection: number) {
-    const currentDirectionIndex = Ship._directionOrder.findIndex(direction => direction === this._currentDirection);
-    let adjustedDirectionIndex = (currentDirectionIndex + numberOfTurnsAndDirection) % Ship._directionOrder.length;
-    adjustedDirectionIndex = adjustedDirectionIndex >= 0 ? adjustedDirectionIndex : Ship._directionOrder.length + adjustedDirectionIndex;
-    this._currentDirection = Ship._directionOrder[adjustedDirectionIndex];
   }
 
   private _rotateWayPoint(angle: number) {
@@ -61,56 +44,28 @@ class Ship {
 
   public processActionsPart01(actions: Action[]) {
     actions.forEach(action => {
-      switch (action.id) {
-        case 'N':
-          this._currentPosition = this._currentPosition.add(this._deriveNavigationPoint(Direction.NORTH, action.value));
-          break;
-        case 'S':
-          this._currentPosition = this._currentPosition.add(this._deriveNavigationPoint(Direction.SOUTH, action.value));
-          break;
-        case 'E':
-          this._currentPosition = this._currentPosition.add(this._deriveNavigationPoint(Direction.EAST, action.value));
-          break;
-        case 'W':
-          this._currentPosition = this._currentPosition.add(this._deriveNavigationPoint(Direction.WEST, action.value));
-          break;
-        case 'L':
-          this._updateDirection(-(action.value / 90));
-          break;
-        case 'R':
-          this._updateDirection(action.value / 90);
-          break;
-        case 'F':
-          this._currentPosition = this._currentPosition.add(this._deriveNavigationPoint(this._currentDirection, action.value));
-          break;
+      if(['N', 'S', 'E', 'W'].includes(action.id)) {
+        this._currentPosition = this._currentPosition.add(this._deriveNavigationPoint(action.id, action.value));
+      } else if(action.id === 'L') {
+        this._rotateWayPoint(action.value);        
+      } else if(action.id === 'R') {
+        this._rotateWayPoint(-action.value);
+      } else if(action.id === 'F') {
+        this._currentPosition = this._currentPosition.add(this._currentWayPoint.multiplyByScalar(action.value));
       }
     });
   }
 
   public processActionsPart02(actions: Action[]) {
     actions.forEach(action => {
-      switch (action.id) {
-        case 'N':
-          this._currentWayPoint = this._currentWayPoint.add(this._deriveNavigationPoint(Direction.NORTH, action.value));
-          break;
-        case 'S':
-          this._currentWayPoint = this._currentWayPoint.add(this._deriveNavigationPoint(Direction.SOUTH, action.value));
-          break;
-        case 'E':
-          this._currentWayPoint = this._currentWayPoint.add(this._deriveNavigationPoint(Direction.EAST, action.value));
-          break;
-        case 'W':
-          this._currentWayPoint = this._currentWayPoint.add(this._deriveNavigationPoint(Direction.WEST, action.value));
-          break;
-        case 'L':
-          this._rotateWayPoint(action.value);
-          break;
-        case 'R':
-          this._rotateWayPoint(-action.value);
-          break;
-        case 'F':
-          this._currentPosition = this._currentPosition.add(this._currentWayPoint.multiplyByScalar(action.value));
-          break;
+      if(['N', 'S', 'E', 'W'].includes(action.id)) {
+        this._currentWayPoint = this._currentWayPoint.add(this._deriveNavigationPoint(action.id, action.value));
+      } else if(action.id === 'L') {
+        this._rotateWayPoint(action.value);        
+      } else if(action.id === 'R') {
+        this._rotateWayPoint(-action.value);
+      } else if(action.id === 'F') {
+        this._currentPosition = this._currentPosition.add(this._currentWayPoint.multiplyByScalar(action.value));
       }
     });
   }
@@ -133,13 +88,13 @@ export class Day12Service {
   }
 
   processActionsAndGetDistancePart01(actions: Action[]) : number {
-    const ship = new Ship();
+    const ship = new Ship(new Point(1,0));
     ship.processActionsPart01(actions);
     return ship.currentPosition.calculateManhattanDistance(new Point(0,0));
   }
 
   processActionsAndGetDistancePart02(actions: Action[]) : number {
-    const ship = new Ship();
+    const ship = new Ship(new Point(10, 1));
     ship.processActionsPart02(actions);
     return ship.currentPosition.calculateManhattanDistance(new Point(0,0));
   }
